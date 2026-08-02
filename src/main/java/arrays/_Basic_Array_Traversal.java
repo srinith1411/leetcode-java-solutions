c272dc1e8d905022165465bb0fd0723c4d7a4cca
Srinith Ailla <188548595+srinith1411@users.noreply.github.com>
2026-08-02 22:07:03 +0530

    Initial Commit

---
+++ src/main/java/arrays/_Basic_Array_Traversal.java
@@ -0,0 +1,13 @@
+package dsa.arrays;
+
+public class _BasicTraversal {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ src/main/java/strings/_BasicProgram.java
@@ -0,0 +1,18 @@
+package dsa.strings;
+
+import java.util.Scanner;
+
+public class _BasicString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("You entered: " + str);
+
+        sc.close();
+    }
+}
