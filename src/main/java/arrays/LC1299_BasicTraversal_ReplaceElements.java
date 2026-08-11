d192cead216ab8879df55e4db90a9ac145ba73b0
Srinith Ailla <188548595+srinith1411@users.noreply.github.com>
2026-08-11 07:42:19 +0530

    Day 10: Solve LC 1299 Replace Elements with Greatest Element on Right Side (Basic Traversal) & LC 14 Longest Common Prefix (Basic Traversal)

---
+++ src/main/java/arrays/LC1299_BasicTraversal_ReplaceElements.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class LC1299_BasicTraversal_ReplaceElements {
+
+    public static int[] replaceElements(int[] a) {
+
+        for(int i=0;i<a.length;i++){
+
+            int max=-1;
+
+            for(int j=i+1;j<a.length;j++){
+
+                if(a[j]>max)
+                    max=a[j];
+            }
+            a[i]=max;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        System.out.println("Enter " + n + " array elements:");
+
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+
+        int[] result = replaceElements(a);
+
+        System.out.println("Array after replacing elements:");
+
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i] + " ");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/main/java/strings/LC14_BasicTraversal_LongestCommonPrefix.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class LC14_BasicTraversal_LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] w) {
+
+        StringBuilder sb = new StringBuilder("");
+
+        for(int i=0;i<w[0].length();i++){
+
+            for(int j=1;j<w.length;j++){
+
+                if(i>=w[j].length() || w[j].charAt(i)!=w[0].charAt(i)){
+                    return new String(sb);
+                }
+            }
+
+            sb.append(w[0].charAt(i));
+        }
+
+        return new String(sb);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of strings: ");
+        int n = sc.nextInt();
+
+        String[] w = new String[n];
+
+        System.out.println("Enter " + n + " strings:");
+
+        for(int i=0;i<n;i++){
+            w[i] = sc.next();
+        }
+
+        String result = longestCommonPrefix(w);
+
+        System.out.println("Longest Common Prefix: " + result);
+
+        sc.close();
+    }
+}
