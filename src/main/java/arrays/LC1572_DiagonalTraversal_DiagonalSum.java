82e23fe38dbb4b4baa1de01762aa362f25469b22
Srinith Ailla <188548595+srinith1411@users.noreply.github.com>
2026-08-13 08:27:59 +0530

    Day 12: Solve LC 917 Reverse Only Letters (Two Pointers) & LC 1572 Matrix Diagonal Sum (Matrix Traversal)

---
+++ src/main/java/arrays/LC1572_DiagonalTraversal_DiagonalSum.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class LC1572_DiagonalTraversal_DiagonalSum {
+
+    public static int diagonalSum(int[][] mat) {
+
+        int s = 0;
+
+        for (int i = 0; i < mat.length; i++) {
+
+            s = s + mat[i][i];
+            s = s + mat[i][mat.length - 1 - i];
+
+        }
+
+        if (mat.length % 2 == 1) {
+
+            s = s - mat[mat.length / 2][mat.length / 2];
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter matrix size: ");
+        int n = sc.nextInt();
+
+        int[][] mat = new int[n][n];
+
+        System.out.println("Enter matrix elements:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Diagonal Sum: " + diagonalSum(mat));
+
+        sc.close();
+    }
+} 
+    
+
---
+++ src/main/java/strings/LC917_TwoPointer_ReverseOnlyLetters.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class LC917_TwoPointer_ReverseOnlyLetters {
+
+    public static String reverseOnlyLetters(String s) {
+
+        char c[] = s.toCharArray();
+
+        int i = 0, j = c.length - 1;
+
+        while (i < j) {
+
+            char l = c[i];
+            char r = c[j];
+
+            if (!Character.isLetter(l) && !Character.isLetter(r)) {
+                i++;
+                j--;
+            }
+
+            else if (Character.isLetter(l) && Character.isLetter(r)) {
+
+                char t = c[i];
+                c[i] = c[j];
+                c[j] = t;
+
+                i++;
+                j--;
+            }
+
+            else if (!Character.isLetter(l) && Character.isLetter(r)) {
+                i++;
+            }
+
+            else if (Character.isLetter(l) && !Character.isLetter(r)) {
+                j--;
+            }
+        }
+
+        return new String(c);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        System.out.println("Result: " + reverseOnlyLetters(s));
+
+        sc.close();
+    }
+}
